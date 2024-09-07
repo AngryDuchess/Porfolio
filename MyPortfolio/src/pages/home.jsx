9ff32f1033fa546ce749f1ec2705b740e17e223b
Mahama Hamida <mahamahamida@gmail.com>
2024-09-07 22:08:51 +0000

    refactor: Add TechStack component to home page

--- MyPortfolio/src/pages/home.jsx
+++ MyPortfolio/src/pages/home.jsx
@@ -2,12 +2,14 @@
 import Works from '../components/Works.jsx'
 import About from '../components/About.jsx'
 import HeroSection from '../components/Hero.jsx'
+import TechStack from '../components/TechStack.jsx'
 
 export default function Home() {
     return(
     <>
     
     <HeroSection />
+    <TechStack />
     <Works />
     {/* <div className="elfsight-app-15429149-2614-4ee4-b451-493b151f462d" data-elfsight-app-lazy></div> */}
     <About />
